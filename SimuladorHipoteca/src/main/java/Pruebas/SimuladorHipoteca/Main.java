d570b4a67f328100e2eee06c52e34132d5f2a576
VASS\mgomezgarrote <mgomezgarrote@serbatic.es>
2022-06-13 13:12:26 +0200

    Prueba 2

--- SimuladorHipoteca/src/main/java/Pruebas/SimuladorHipoteca/Main.java
+++ SimuladorHipoteca/src/main/java/Pruebas/SimuladorHipoteca/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main( String[] args ){
     	  	
     	Scanner sc = new Scanner(System.in);
-        System.out.println( "Elija tipo de interés (1/2)" );
+    	System.out.println("Meto una linea de algo");
+        System.out.println( "Elija tipo de interï¿½s (1/2)" );
         System.out.println("1. Tipo fijo");
         System.out.println("2. Tipo variable");
         int opcion = sc.nextInt();
@@ -14,7 +15,7 @@ public class Main {
         System.out.println("Cantidad total a amortizar:");
         float cantidadTotal = sc.nextFloat();
         
-        System.out.println("Número de años:");
+        System.out.println("Nï¿½mero de aï¿½os:");
         int anios = sc.nextInt();
         
         if (opcion == 1) { 
